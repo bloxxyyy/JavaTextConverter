aeab8ce181bb8ddf32743279889754e287390fdc
Marco Noppen <mn.noppen@hotmail.com>
2020-04-06 16:27:55 +0200

    Updated Conv.java
    * Code cleanup
    * switch statement in the post
    * extra method for code which has some more functionality.

--- src/java/Conv/Conv.java
+++ src/java/Conv/Conv.java
@@ -1,9 +1,5 @@
 package Conv;
 
-/*
- * Copyright (C) 2020 Mark Blokker ~ Ad-Blokker
- */
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Random;
@@ -13,159 +9,126 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * @author Mark Blokker ~ Ad-Blokker
- */
 @WebServlet(urlPatterns = {"/Conv"})
 public class Conv extends HttpServlet {
+
+    private string CheckAltInval(String input) {
+        
+        String output = "";
+        
+        for (int i = 0; i < input.length(); i++) {
+
+            char text = input.charAt(i);
+            
+            if (Character.isWhitespace(text)) {
+                output += input.charAt(i);
+                    if (i + 1 < input.length()) i++;              
+            }
+            
+            output += convtype.equals("invaltcase") : input.charAt(i) ? input.toUpperCase().charAt(i);
+            
+            if (i + 1 < input.length()) {
+                i++;
+                text = input.charAt(i);
+                
+                
+                if (Character.isWhitespace(text))
+                    output += input.charAt(i);
+                    if (i + 1 < input.length()) i++;
+                }
+                
+                output += convtype.equals("invaltcase") : input.toUpperCase().charAt(i) ? input.charAt(i);
+            }
+        }
+        
+        return output;
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Content-Type", "text/plain; charset=utf-8");
         request.setCharacterEncoding("UTF-8");
+        
         String input = request.getParameter("input");
         String convtype = request.getParameter("format");
         String output = "";
-        boolean validConvType = false;
-
-        // <editor-fold defaultstate="collapsed" desc="ALL CAPS. Click on the + sign on the left to edit the code.">
-        if (convtype.equals("caps")) {
-            validConvType = true;
-            output = input.toUpperCase();
-        }
-        // </editor-fold>
         
-        // <editor-fold defaultstate="collapsed" desc="Alternating and Inverted alternating Case. Click on the + sign on the left to edit the code.">
-        if (convtype.equals("altcase") || convtype.equals("invaltcase")) {
-            input = input.toLowerCase();
-            validConvType = true;
-            for (int i = 0; i < input.length(); i++) {
-
-                char test = input.charAt(i);
-                if (Character.isWhitespace(test)) {
-                    output += input.charAt(i);
-                    if (i + 1 < input.length()) {
-                        i++;
-                    }                     
+        input = input.toLowerCase();
+        
+        boolean valid = true;
+        
+        switch (convtype) {
+            case "caps":
+                output = input.toUpperCase();
+            break;
+                
+            case "altcase":
+                output = CheckAltInval(input);
+            break;
+                
+            case "invaltcase":
+                output = CheckAltInval(input);
+            break;
+                
+            case "randomcase":
+                Random coin = new Random();  
+                for (int i = 0; i < input.length(); i++) {
+                    output += (coin.nextInt(2) == 0) : input.toUpperCase().charAt(i) ? input.charAt(i);
                 }
-                if (convtype.equals("invaltcase")) {
-                    output += input.charAt(i);
-                } else { //altcase
-                    output += input.toUpperCase().charAt(i);
-                }
-                if (i + 1 < input.length()) {
-                    i++;
-                    test = input.charAt(i);
-                    if (Character.isWhitespace(test)) {
-                        output += input.charAt(i);
+            break;
+                
+            case "leet":
+                Leet l33t = new Leet();
+                l33t.leetConv(input);
+                output = l33t.getLeet();
+            break;
+                
+            case "angrycase":
+                boolean lastCharDot = false;
+                output += input.toUpperCase().charAt(0);
+                
+                for (int i = 1; i < input.length(); i++) {
+                    char text = input.charAt(i);
+                    if (text == '.') continue; 
+                    
+                    if (Character.isWhitespace(text)) {
+                        output += ". ";
                         if (i + 1 < input.length()) {
                             i++;
-                        }
-                    }
-                    if (convtype.equals("invaltcase")) {
-                        output += input.toUpperCase().charAt(i);
-                    } else { //altcase
+                            output += input.toUpperCase().charAt(i);
+                        }                     
+                    } else {
                         output += input.charAt(i);
                     }
+                    
+                    text = input.charAt(i);
+                    lastCharDot = text == '.' || test == '!' || test == '?';
+                   
                 }
-            }
-        }
-        
-        // </editor-fold>
-        
-        // <editor-fold defaultstate="collapsed" desc="Random Case. Click on the + sign on the left to edit the code.">
-        if (convtype.equals("randomcase")) {
-            input = input.toLowerCase();
-            validConvType = true;
-            Random coin = new Random();  
-
-            for (int i = 0; i < input.length(); i++) {
-                if (coin.nextInt(2) == 0) { //change to higher number and try to dived by 2
-                    output += input.toUpperCase().charAt(i);
-                } else {
-                    output += input.charAt(i);
-                }
-            }
-        }
-        // </editor-fold>
-        
-        // <editor-fold defaultstate="collapsed" desc="Leetspeak. Click on the + sign on the left to edit the code.">
-        if (convtype.equals("leet")) {
-            input = input.toLowerCase();
-            validConvType = true;
-            Leet l33t = new Leet();
-            l33t.leetConv(input.toLowerCase());
-            output = l33t.getLeet();
-        }
-        
-        // </editor-fold>
-        
-        // <editor-fold defaultstate="collapsed" desc="Angry case. Click on the + sign on the left to edit the code.">
-        if (convtype.equals("angrycase")) {
-            validConvType = true;
-            boolean lastCharDot = false;
-            output += input.toUpperCase().charAt(0);
-            for (int i = 1; i < input.length(); i++) {
-                char test = input.charAt(i);
-                if (test == '.'){
-                    continue;
-                }
-                if (Character.isWhitespace(test)) {
-                    output += ". ";
-                    if (i + 1 < input.length()) {
-                        i++;
-                        output = output + input.toUpperCase().charAt(i);
-                    }                     
-                } else {
-                    output += input.charAt(i);
-                }
-                test = input.charAt(i);
-                lastCharDot = test == '.' || test == '!' || test == '?';
-               
-            }
-            if (!lastCharDot){
-                output += ".";
-            }
+                if (!lastCharDot) output += ".";
+            break;
             
+            default:
+                valid = false;
+            break;
         }
-        // </editor-fold>
         
-        
-        // <editor-fold defaultstate="collapsed" desc="Print out. Click on the + sign on the left to edit the code.">
 
         try (PrintWriter writer = response.getWriter()) {
-            if (validConvType) {
-                writer.print(output);
-            } else {
+            if (valid) writer.print(output);
+            else {
                 response.setStatus(400);
                 writer.print("You dum-dum, conversion type no exist. :(");
             }
         }
-        // </editor-fold>
-        
     }
 
-    /**
-     * Returns a short description of the servlet.
-     * @return a String containing servlet description
-     */
     @Override
-    public String getServletInfo() {
-        return "CaseConverter";
-    }
+    public String getServletInfo() => return "CaseConverter";
     
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         
         PrintWriter out = response.getWriter();
